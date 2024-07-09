809fe5022f6f4365e2bbf98e19c4bf3f72ecb2dd
Nolwazi Ngcongo <ngcongonolwazy@gmail.com>
2024-07-09 18:16:18 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>
        Weather App
       </h1>
       <footer>
-        This project was coded by <a href="https://portfolio-velvety-5418a3.netlify.app" target="_blank">Nolwazi Ngcongo</a> and is open-sourced on {" "} <a href="https://github.com/NolwaziNgcongo/react-weather-app" target="_blank">GitHub</a>
+        This project was coded by <a href="https://portfolio-velvety-5418a3.netlify.app" target="_blank" rel="noreferrer">Nolwazi Ngcongo</a> and is open-sourced on {" "} <a href="https://github.com/NolwaziNgcongo/react-weather-app" target="_blank" rel="noreferrer">GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
