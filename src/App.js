3640b2e222419894411bbdeb537fbf6b08b4a369
Nolwazi Ngcongo <ngcongonolwazy@gmail.com>
2024-07-09 17:51:34 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>
+       Weather App
+      </h1>
+      <footer>
+        This project was coded by <a href="https://portfolio-velvety-5418a3.netlify.app" target="_blank">Nolwazi Ngcongo</a> and is open-sourced on {" "} <a href="https://github.com/NolwaziNgcongo/react-weather-app" target="_blank">GitHub</a>
+      </footer>
     </div>
   );
 }
