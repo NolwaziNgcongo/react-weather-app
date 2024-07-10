45ada6297059309cbfdd945ffd281a79be98665a
Nolwazi Ngcongo <ngcongonolwazy@gmail.com>
2024-07-10 11:24:31 +0200

    Added structure of Weather file and CSS

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
@@ -7,8 +9,9 @@ export default function App() {
       <h1>
        Weather App
       </h1>
+      <Weather />
       <footer>
-        This project was coded by <a href="https://portfolio-velvety-5418a3.netlify.app" target="_blank" rel="noreferrer">Nolwazi Ngcongo</a> and is open-sourced on {" "} <a href="https://github.com/NolwaziNgcongo/react-weather-app" target="_blank" rel="noreferrer">GitHub</a>
+        This project was coded by <a href="https://portfolio-velvety-5418a3.netlify.app" target="_blank" rel="noreferrer">Nolwazi Ngcongo</a> and is open-sourced on {" "} <a href="https://github.com/NolwaziNgcongo/react-weather-app" target="_blank" rel="noreferrer">GitHub</a>{" "}and <a href="https://incandescent-empanada-3f5eb3.netlify.app" target="_blank" rel="noopener noreferrer">hosted on Netlify</a>
       </footer>
       </div>
     </div>
