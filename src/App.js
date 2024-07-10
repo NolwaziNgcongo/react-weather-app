909b00f4dc9c3614fbb7e92c748d865e754eb6b9
Nolwazi Ngcongo <ngcongonolwazy@gmail.com>
2024-07-10 11:58:54 +0200

    Added  all weather information into different files and added CSS files to the project

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>
-       Weather App
-      </h1>
-      <Weather />
+      <Weather defaultCity="Greece" />
       <footer>
         This project was coded by <a href="https://portfolio-velvety-5418a3.netlify.app" target="_blank" rel="noreferrer">Nolwazi Ngcongo</a> and is open-sourced on {" "} <a href="https://github.com/NolwaziNgcongo/react-weather-app" target="_blank" rel="noreferrer">GitHub</a>{" "}and <a href="https://incandescent-empanada-3f5eb3.netlify.app" target="_blank" rel="noopener noreferrer">hosted on Netlify</a>
       </footer>
